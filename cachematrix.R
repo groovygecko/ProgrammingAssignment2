1caa6f37a990ddcf22cd8d879590656fdabecc0b
Steven Merriel <steven.merriel@gmail.com>
2015-05-24 23:26:25 +1000

    Submission of assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function takes a matrix known as 'x', calculates the inverse matrix and assigns this
+## inverse to a global free variable so that it can be utilised at a later time
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(b) {
+        a <<- b
+        m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function attempts to find a stored copy of the inverse matrix in the global 'm'
+## variable. If it is not found then the function calulates the inverse and returns 
+## the result.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {  
+      m <- a$getinverse()
+      If(!is.null(m)) {
+        message("Returning cached inverse matrix")
+        return(m)
+      }
+      data <- a$get()
+      m <- solve(data, ...)
+      a$setinverse(m)
+      m
 }
